6dd2132d8c77c47238816fe3718ad311f7fae2d2
za3a24 <za3a24@yandex.ru>
2013-06-21 19:25:51 +0700

    Moved test classes to same packages as their original classes

--- Assets/Test/tdp/entity/behaviour/bullet/movement/BulletMoveToRightTest.cs
+++ Assets/Test/tdp/entity/behaviour/bullet/movement/BulletMoveToRightTest.cs
@@ -1,28 +1,27 @@
 ﻿using Assets.Scripts.tdp.configuration;
 using Assets.Scripts.tdp.entity;
-using Assets.Scripts.tdp.entity.behaviour.bullet;
 using Assets.Scripts.tdp.entity.behaviour.bullet.movement;
 using Assets.Test.utility;
 using NUnit.Framework;
 using UnityEngine;
 
-namespace Assets.Test.tdp.entity.behaviour.bullet {
+namespace Assets.Test.tdp.entity.behaviour.bullet.movement {
     [TestFixture]
     public class BulletMoveToRightTest {
         private Bullet testBullet;
-        
+
         [SetUp]
         public void SetUp() {
             
             testBullet =
-                ScriptInstantiator.InstantiateScript<Bullet>((GameObject) Resources.Load("Prefabs/Entities/BulletPrefab"));
+                ScriptInstantiator.InstantiateScript<Bullet>(
+                    (GameObject) Resources.Load("Prefabs/Entities/BulletPrefab"));
             testBullet.movementStrategy = new MoveToRight();
             testBullet.speed = Configuration.BulletMovementSpeed;
         }
 
         [Test]
         public void MoveTest() {
-            //Debug.Log("BulletMoveToRight Test");
             Vector3 oldPosition = testBullet.transform.position;
             testBullet.movementStrategy.Move(testBullet, 100.0f);
             Vector3 newPosition = testBullet.transform.position;
--- Assets/Test/tdp/entity/behaviour/enemy/movement/EnemyMoveToLeftTest.cs
+++ Assets/Test/tdp/entity/behaviour/enemy/movement/EnemyMoveToLeftTest.cs
@@ -1,31 +1,25 @@
 ﻿using Assets.Scripts.tdp.configuration;
 using Assets.Scripts.tdp.constants;
 using Assets.Scripts.tdp.entity;
-using Assets.Scripts.tdp.entity.behaviour.enemy;
 using Assets.Scripts.tdp.entity.behaviour.enemy.death;
 using Assets.Scripts.tdp.entity.behaviour.enemy.movement;
 using Assets.Test.utility;
 using NUnit.Framework;
 using UnityEngine;
 
-namespace Assets.Test.tdp.entity.behaviour.enemy {
+namespace Assets.Test.tdp.entity.behaviour.enemy.movement {
     [TestFixture]
     public class EnemyMoveToLeftTest {
         private Enemy testEnemy;
-        
+
         [SetUp]
         public void SetUp() {
             testEnemy =
-                ScriptInstantiator.InstantiateScript<Enemy>((GameObject)Resources.Load("Prefabs/Entities/EnemyPrefab"));
+                ScriptInstantiator.InstantiateScript<Enemy>(
+                    (GameObject)Resources.Load("Prefabs/Entities/EnemyPrefab"));
             testEnemy.movementStrategy = new MoveToLeft();
             testEnemy.deathStrategy = new EnemyDeath();
             testEnemy.speed = Configuration.Enemies[EnemyType.Type1].Speed;
-        }
-
-        [TearDown]
-        public void TearDown() {
-            Object.DestroyImmediate(testEnemy.gameObject);
-            ScriptInstantiator.CleanUp();
         }
 
         [Test]
@@ -35,6 +29,12 @@ namespace Assets.Test.tdp.entity.behaviour.enemy {
             Vector3 newPosition = testEnemy.transform.position;
 
             Assert.That(newPosition.x, Is.LessThan(oldPosition.x));
+        }
+
+        [TearDown]
+        public void TearDown() {
+            Object.DestroyImmediate(testEnemy.gameObject);
+            ScriptInstantiator.CleanUp();
         }
     }
 }
