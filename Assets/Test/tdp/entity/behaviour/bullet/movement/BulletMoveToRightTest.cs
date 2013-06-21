bb66d213fbb4a23d2b26f9685107d57c17d71952
za3a24 <za3a24@yandex.ru>
2013-06-21 19:27:04 +0700

    Improving movement tests making them independent from configuration and replacing criteria

--- Assets/Test/tdp/entity/behaviour/bullet/movement/BulletMoveToRightTest.cs
+++ Assets/Test/tdp/entity/behaviour/bullet/movement/BulletMoveToRightTest.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Scripts.tdp.configuration;
-using Assets.Scripts.tdp.entity;
+﻿using Assets.Scripts.tdp.entity;
 using Assets.Scripts.tdp.entity.behaviour.bullet.movement;
 using Assets.Test.utility;
 using NUnit.Framework;
@@ -9,28 +8,31 @@ namespace Assets.Test.tdp.entity.behaviour.bullet.movement {
     [TestFixture]
     public class BulletMoveToRightTest {
         private Bullet testBullet;
+        private const float BulletMovementSpeed = 1.0f;
+        private const float BulletMovementTime = 100.0f;
 
         [SetUp]
         public void SetUp() {
-            
             testBullet =
                 ScriptInstantiator.InstantiateScript<Bullet>(
                     (GameObject) Resources.Load("Prefabs/Entities/BulletPrefab"));
             testBullet.movementStrategy = new MoveToRight();
-            testBullet.speed = Configuration.BulletMovementSpeed;
+            testBullet.speed = BulletMovementSpeed;
         }
 
         [Test]
         public void MoveTest() {
             Vector3 oldPosition = testBullet.transform.position;
-            testBullet.movementStrategy.Move(testBullet, 100.0f);
+            testBullet.movementStrategy.Move(testBullet, BulletMovementTime);
             Vector3 newPosition = testBullet.transform.position;
 
-            Assert.That(newPosition.x, Is.GreaterThan(oldPosition.x));
+            Assert.That(newPosition.x,
+                        Is.EqualTo(oldPosition.x + BulletMovementSpeed * BulletMovementTime));
         }
 
         [TearDown]
-        public void TearDown() {
+        public void TearDown()
+        {
             Object.DestroyImmediate(testBullet.gameObject);
             ScriptInstantiator.CleanUp();
         }
--- Assets/Test/tdp/entity/behaviour/enemy/movement/EnemyMoveToLeftTest.cs
+++ Assets/Test/tdp/entity/behaviour/enemy/movement/EnemyMoveToLeftTest.cs
@@ -1,6 +1,4 @@
-﻿using Assets.Scripts.tdp.configuration;
-using Assets.Scripts.tdp.constants;
-using Assets.Scripts.tdp.entity;
+﻿using Assets.Scripts.tdp.entity;
 using Assets.Scripts.tdp.entity.behaviour.enemy.death;
 using Assets.Scripts.tdp.entity.behaviour.enemy.movement;
 using Assets.Test.utility;
@@ -11,30 +9,35 @@ namespace Assets.Test.tdp.entity.behaviour.enemy.movement {
     [TestFixture]
     public class EnemyMoveToLeftTest {
         private Enemy testEnemy;
+        private const float EnemyMovementSpeed = 1.0f;
+        private const float EnemyMovementTime = 100.0f;
 
         [SetUp]
         public void SetUp() {
             testEnemy =
                 ScriptInstantiator.InstantiateScript<Enemy>(
-                    (GameObject)Resources.Load("Prefabs/Entities/EnemyPrefab"));
+                    (GameObject) Resources.Load("Prefabs/Entities/EnemyPrefab"));
             testEnemy.movementStrategy = new MoveToLeft();
             testEnemy.deathStrategy = new EnemyDeath();
-            testEnemy.speed = Configuration.Enemies[EnemyType.Type1].Speed;
+            testEnemy.speed = EnemyMovementSpeed;
         }
 
         [Test]
         public void MoveTest() {
             Vector3 oldPosition = testEnemy.transform.position;
-            testEnemy.movementStrategy.Move(testEnemy, 100.0f);
+            testEnemy.movementStrategy.Move(testEnemy, EnemyMovementTime);
             Vector3 newPosition = testEnemy.transform.position;
 
-            Assert.That(newPosition.x, Is.LessThan(oldPosition.x));
+            Assert.That(newPosition.x,
+                        Is.EqualTo(oldPosition.x - EnemyMovementSpeed * EnemyMovementTime));
         }
 
         [TearDown]
-        public void TearDown() {
+        public void TearDown()
+        {
             Object.DestroyImmediate(testEnemy.gameObject);
             ScriptInstantiator.CleanUp();
         }
+
     }
 }
