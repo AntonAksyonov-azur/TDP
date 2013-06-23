d4cc93396eace2547292c4d79704113ba49360be
za3a24 <za3a24@yandex.ru>
2013-06-23 15:14:30 +0700

    Removed unnecessary commentaries from DestroyBulletTest

---
+++ Assets/Tests/tdp/entity/bullet/behaviour/destruction/DestroyBulletTest.cs
@@ -0,0 +1,40 @@
+﻿using Assets.Scripts.tdp.entity.bullet;
+using Assets.Scripts.tdp.entity.bullet.behaviour.destruction;
+using Assets.Tests.utility;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Tests.tdp.entity.bullet.behaviour.destruction {
+
+    [TestFixture]
+    public class DestroyBulletTest {
+        private Bullet testBullet;
+
+        [SetUp]
+        public void SetUp() {
+            testBullet =
+                ScriptInstantiator.InstantiateScript<Bullet>(
+                    (GameObject) Resources.Load("Prefabs/Entities/BulletPrefab"));
+            testBullet.destroyStrategy = new DestroyBullet();
+        }
+
+        [Test]
+        public void DestroyTest() {
+            Assert.NotNull(testBullet);
+
+            testBullet.destroyStrategy.Destroy(testBullet);
+
+            Assert.Null(testBullet.sprite);
+
+        }
+
+        [TearDown]
+        public void TearDown() {
+            // Если тест провалился, ресурсы все равно должны быть очищены
+            if (testBullet != null) {
+                Object.DestroyImmediate(testBullet);
+            }
+            ScriptInstantiator.CleanUp();
+        }
+    }
+}
