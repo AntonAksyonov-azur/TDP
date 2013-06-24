c68b51da8d16f78aa955a692c0a0b77791a56f5b
za3a24 <za3a24@yandex.ru>
2013-06-24 12:00:49 +0700

    Remove rectangle creating in every OnGUI call (TowerSlot script)

--- Assets/Scripts/tdp/gui/TowerSlot.cs
+++ Assets/Scripts/tdp/gui/TowerSlot.cs
@@ -14,46 +14,57 @@ namespace Assets.Scripts.tdp.gui {
 
         private int status;
         private Vector2 cachedPosition;
-        private float cachedSize;
+        private Rect cachedRectangleMainButton;
+
+        private float cachedSizeForTypeButtons;
+        private Rect cachedRectangleTypeButton1;
+        private Rect cachedRectangleTypeButton2;
+        private Rect cachedRectangleTypeButton3;
 
         public void Start() {
             cachedPosition = CoordinateConverter.RealCoordinatesToScreen(
                 Configuration.ScreenWidth, Configuration.ScreenHeight,
                 transform.position.x, transform.position.y);
-            cachedSize = Configuration.TowerSlotHeight / 3;
+
+            cachedRectangleMainButton = new Rect(
+                cachedPosition.x - Configuration.TowerSlotWidth / 2,
+                cachedPosition.y - Configuration.TowerSlotHeight / 2,
+                Configuration.TowerSlotWidth,
+                Configuration.TowerSlotHeight);
+
+            cachedSizeForTypeButtons = Configuration.TowerSlotHeight / 3;
+            cachedRectangleTypeButton1 = new Rect(
+                cachedPosition.x - Configuration.TowerSlotWidth / 2,
+                cachedPosition.y - Configuration.TowerSlotHeight / 2,
+                Configuration.TowerSlotWidth,
+                cachedSizeForTypeButtons);
+            cachedRectangleTypeButton2 = new Rect(
+                cachedPosition.x - Configuration.TowerSlotWidth / 2,
+                cachedPosition.y - Configuration.TowerSlotHeight / 2 + cachedSizeForTypeButtons,
+                Configuration.TowerSlotWidth,
+                cachedSizeForTypeButtons);
+            cachedRectangleTypeButton3 = new Rect(
+                cachedPosition.x - Configuration.TowerSlotWidth / 2,
+                cachedPosition.y - Configuration.TowerSlotHeight / 2 + cachedSizeForTypeButtons * 2,
+                Configuration.TowerSlotWidth,
+                cachedSizeForTypeButtons);
         }
 
         public void OnGUI() {
             if (status == INITIAL_STATUS) {
-                if (GUI.Button(new Rect(
-                                   cachedPosition.x - Configuration.TowerSlotWidth / 2,
-                                   cachedPosition.y - Configuration.TowerSlotHeight / 2,
-                                   Configuration.TowerSlotWidth,
-                                   Configuration.TowerSlotHeight), "Build\nTower")) {
+                if (GUI.Button(cachedRectangleMainButton, "Build\nTower")) {
                     status = CHOOSE_TYPE_OF_TOWER_STATUS;
                 }
             }
 
             if (status == CHOOSE_TYPE_OF_TOWER_STATUS) {
-                if (GUI.Button(new Rect(
-                                   cachedPosition.x - Configuration.TowerSlotWidth / 2,
-                                   cachedPosition.y - (Configuration.TowerSlotHeight / 2),
-                                   Configuration.TowerSlotWidth,
-                                   cachedSize), "Type 1")) {
+                if (GUI.Button(cachedRectangleTypeButton1, "Type 1")) {
                     CreateTowerAndDestroySlot(TowerType.Type1);
                 }
-                if (GUI.Button(new Rect(
-                                   cachedPosition.x - Configuration.TowerSlotWidth / 2,
-                                   cachedPosition.y - Configuration.TowerSlotHeight / 2 + cachedSize,
-                                   Configuration.TowerSlotWidth,
-                                   cachedSize), "Type 2")) {
+                if (GUI.Button(cachedRectangleTypeButton2, "Type 2")) {
                     CreateTowerAndDestroySlot(TowerType.Type2);
                 }
-                if (GUI.Button(new Rect(
-                                   cachedPosition.x - Configuration.TowerSlotWidth / 2,
-                                   cachedPosition.y - Configuration.TowerSlotHeight / 2 + cachedSize * 2,
-                                   Configuration.TowerSlotWidth,
-                                   cachedSize), "Type 3")) {
+                if (GUI.Button(cachedRectangleTypeButton3, "Type 3")) {
                     CreateTowerAndDestroySlot(TowerType.Type3);
                 }
             }
